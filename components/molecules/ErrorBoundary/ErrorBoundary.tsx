2dee69560f80dc23b050da98046247e5d5c76f44
Nihad Tahunic <ntahunic@gmail.com>
2022-11-24 00:48:35 +0100

    Add ErrorBoundary

---
+++ components/molecules/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+// @ts-nocheck
+import React, { ErrorInfo, ReactElement } from 'react';
+
+interface StateType {
+  hasError: boolean;
+}
+
+interface PropsType {
+  children: ReactElement;
+}
+
+export class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Log the error to the backend
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import '../styles/globals.css'
+import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'theme-ui';
-import { theme } from '../styles/theme'
+import { theme } from '@styles/theme'
+import { ErrorBoundary } from '@components/molecules/ErrorBoundary';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
     </ThemeProvider>
   );
 }
