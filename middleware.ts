fa0f302345634f563b923ba2ead6a9c1a244c57c
Nihad Tahunic <ntahunic@gmail.com>
2022-11-27 15:55:49 +0100

    Update default route

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone()
   if (url.pathname === '/') {
-    url.pathname = '/buy-orders'
+    url.pathname = '/datasets'
     return NextResponse.redirect(url)
   }
 }
